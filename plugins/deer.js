626133b2923445787e6bcab01a8065dfe4115cd8
Tyson Cleary <tyscorp@gmail.com>
2014-04-05 01:14:12 +1100

    added deer

---
+++ plugins/deer.js
@@ -0,0 +1,31 @@
+var baseStr = "\n%col1%########%col2%#%col1%##%col2%#%col1%#\n%col1%########%col2%#%col1%##%col2%#%col1%#\n%col1%#########%col2%##%col1%##\n%col1%########%col2%###%col1%##\n%col1%#########%col2%##%col1%##\n%col1%##%col2%#########%col1%##\n%col1%#%col2%##########%col1%##\n%col1%#%col2%##########%col1%##\n%col1%#%col2%#%col1%#%col2%#%col1%####%col2%#%col1%#%col2%#%col1%##\n%col1%#%col2%#%col1%#%col2%#%col1%####%col2%#%col1%#%col2%#%col1%##\n%col1%#%col2%#%col1%#%col2%#%col1%####%col2%#%col1%#%col2%#%col1%##";
+
+module.exports = function (bot) {
+    bot.on("cmd_deer", function (nick, to, text) {
+        var col1 = -1;
+        var col2 = -1;
+        var MAX_COLOUR = 15;
+
+        if (parameters.split(' ').length === 2)
+        {
+            col1 = Number(parameters.split(' ')[0]);
+            col2 = Number(parameters.split(' ')[1]);
+        }
+
+        if (col1 < 0 || col1 > MAX_COLOUR || isNaN(col1))
+        {
+            col1 = Math.floor(Math.random()*MAX_COLOUR);
+        }
+
+        if (col2 < 0 || col2 > MAX_COLOUR || isNaN(col2))
+        {
+            col2 = col1;
+            while (col2 === col1)
+            {
+                col2 = Math.floor(Math.random()*MAX_COLOUR);
+            }
+        }
+            
+        bot.say(to, baseStr.replace(/%col1%/g, col1 + "," + col1).replace(/%col2%/g, col2 + "," + col2));
+    });
+};
