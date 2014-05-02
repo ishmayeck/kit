df87c30806b7a35ff9ed2db29504396338a8429b
Tyson Cleary <tyscorp@gmail.com>
2014-05-02 21:34:56 +1000

    fix jpwebm

--- plugins/jpwebm.js
+++ plugins/jpwebm.js
@@ -1,3 +1,5 @@
+var request = require("request");
+
 var remove_derp = function (arr) {
     var i, j, cur, found;
     for (i = arr.length - 1; i >= 0; i--) {
@@ -17,88 +19,92 @@ var remove_derp = function (arr) {
 
 var url = "";
 var webm_list;
+var bot;
 
 function DoThing()
 {
-	request({
-			method: "GET",
-			uri: url,
-		},
-		function (error, response, body) {
-			if(!error && response.statusCode == 404)
-			{
-				console.log("404");
+    request({
+            method: "GET",
+            uri: url,
+        },
+        function (error, response, body) {
+            if(!error && response.statusCode == 404)
+            {
+                console.log("404");
 
-				request({
-						method: "GET",
-						url: "https://a.4cdn.org/jp/catalog.json",
-					    headers: {
-					        'User-Agent': 'request'
-					    }
-					},
-					function (error, response, body) {
-						if(!error)
-						{
-							var base = JSON.parse(body);
-							for(var i = 0; i < 11; i++)
-							{
-								var threads = base[i]["threads"];
-								for(var j = 0; j < threads.length; j++)
-								{
-									thread = threads[j];
-									if(thread["sub"])
-									{
-										var patt = /(AKB|akb).*(G|g)eneral/g;
+                DoOtherThing();
+            }
+            else if(!error)
+            {
+                var html = body;
+                var pattern = /\/\/i.4cdn.org\/jp\/\d*.webm/g;
+                var links = html.match(pattern);
+                if (links && links.length > 0) {
+                    remove_derp(links);
 
-										if(patt.test(thread["sub"]))
-										{
-											url = "http://boards.4chan.org/jp/res/" + thread["no"];
-											console.log(url);
-										}
-									}
-								}
-							}
-						}
-					}
-				);
-			}
-			else if(!error)
-			{
-				var html = body;
-				var pattern = /\/\/i.4cdn.org\/jp\/src\/\d*.webm/g;
-				var links = html.match(pattern);
-				remove_derp(links);
+                    if(!webm_list)
+                    {
+                        //console.log("webm_list was empty, setting to links");
+                        webm_list = links.slice(0, -1); // show the last one on startup for funzies
+                    }
 
-				if(!webm_list)
-				{
-					console.log("webm_list was empty, setting to links");
-					webm_list = links;
-				}
-				else
-				{
-					console.log("webm_list has shit");
-					if(webm_list == links)
-					{
-						console.log("no change");
-					}
-					else
-					{
-						var diff = links.length - webm_list.length;
-						for(var x = webm_list.length; x < webm_list.length + diff; x++)
-						{
-							// put kit saying shit here with links[x] 
-							// but have to put "http:" before the of it because
-							// links are has "//derp.derp.derp/e3189382etc" 
-						}
-						webm_list = links;
-					}
-				}	
-			}
-			else
-			{
-				console.log(error);
-			}
-	});
+                    var diff = links.length - webm_list.length;
+                    for(var x = webm_list.length; x < webm_list.length + diff; x++)
+                    {
+                        //console.log('#test', 'http:' + webm_list[x])
+                        bot.say('#test', 'http:' + links[x]);
+                    }
+                    webm_list = links;
+                }
+            }
+            else
+            {
+                console.log(error);
+            }
+    });
 }
 
+function DoOtherThing() {
+    request({
+        method: "GET",
+        url: "https://a.4cdn.org/jp/catalog.json",
+        headers: {
+            'User-Agent': 'request'
+        }
+    },
+    function (error, response, body) {
+        if(!error)
+        {
+            var base = JSON.parse(body);
+
+            for(var i = 0; i < 11; i++)
+            {
+                if (base[i]) {
+                    var threads = base[i]["threads"];
+                    for(var j = 0; j < threads.length; j++)
+                    {
+                        thread = threads[j];
+                        if(thread["sub"])
+                        {
+                            var patt = /akb.*general/ig;
+
+                            if(patt.test(thread["sub"]))
+                            {
+                                url = "http://boards.4chan.org/jp/res/" + thread["no"];
+                                console.log(url);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    });
+}
+
+DoOtherThing();
+
 setInterval(DoThing, 10000);
+
+module.exports = function (b) {
+    bot = b;
+};
