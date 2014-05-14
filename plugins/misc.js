b2828ae99820db5186fe8c1a3c54077c3deaa408
Tyson Cleary <tyscorp@gmail.com>
2014-05-14 17:18:51 +1000

    aamin

--- plugins/misc.js
+++ plugins/misc.js
@@ -2,4 +2,8 @@ module.exports = function (bot) {
     bot.on('cmd_usoda', function (nick, to, text) { 
         bot.say(to, 'http://feati.funpic.de/usoda.html');
     });
+
+    bot.on('cmd_aamin', function (nick, to, text) { 
+        bot.say(to, 'http://feati.funpic.de/aamin.html');
+    });
 };
