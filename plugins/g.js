76cd06d9c1e74028f3c0c8908b600ce734d42aba
Tyson Cleary <tyscorp@gmail.com>
2014-05-19 14:39:45 +1000

    og

---
+++ plugins/g.js
@@ -0,0 +1,5 @@
+module.exports = function (bot) {
+    bot.on('cmd_g', function (nick, to, text) {
+        bot.say(to, text.replace(/[a-z]/g, 'o').replace(/[A-Z]/g, 'G'));
+    });
+};
--- plugins/o.js
+++ plugins/o.js
@@ -1,6 +1,5 @@
 module.exports = function (bot) {
     bot.on('cmd_o', function (nick, to, text) {
-        var t = text.replace(/[aeiu]/g, 'o').replace(/[AEIU]/g, 'O');
-        bot.say(to, t);
+        bot.say(to, text.replace(/[aeiu]/g, 'o').replace(/[AEIU]/g, 'O'));
     });
 };
