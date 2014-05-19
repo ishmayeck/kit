843f1b8a78df500023c4d2fef0ce08b11cfd889f
Tyson Cleary <tyscorp@gmail.com>
2014-05-19 14:40:38 +1000

    gg

--- plugins/g.js
+++ plugins/g.js
@@ -1,5 +1,5 @@
 module.exports = function (bot) {
     bot.on('cmd_g', function (nick, to, text) {
-        bot.say(to, text.replace(/[a-z]/g, 'o').replace(/[A-Z]/g, 'G'));
+        bot.say(to, text.replace(/[a-z]/g, 'g').replace(/[A-Z]/g, 'G'));
     });
 };
